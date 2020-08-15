1e72b734801bd681411eae20f9159fff54e6cf16
lucasfdcampos <lucascampos.ads@gmail.com>
2020-08-15 17:55:09 -0300

    Finished. All tests passed.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,9 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('foods');
+
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +39,11 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const { data } = await api.post('foods', food);
+
+      const newFoodWithAvailability = { ...data, available: true };
+
+      setFoods([...foods, newFoodWithAvailability]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +52,34 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const updatedFood = {
+      id: editingFood.id,
+      available: editingFood.available,
+      ...food,
+    };
+
+    await api.put(`foods/${updatedFood.id}`, updatedFood);
+
+    const updatedFoodList = foods.map(oldFood => {
+      if (oldFood.id === editingFood.id) {
+        return updatedFood;
+      }
+
+      return oldFood;
+    });
+
+    setFoods(updatedFoodList);
+    setEditingFood({} as IFoodPlate);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`foods/${id}`);
+
+    const indexToBeDeleted = foods.findIndex(food => food.id === id);
+
+    foods.splice(indexToBeDeleted, 1);
+
+    setFoods([...foods]);
   }
 
   function toggleModal(): void {
@@ -62,7 +91,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    setEditModalOpen(true);
   }
 
   return (
